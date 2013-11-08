c5544c2fcbd3cd53befb34e109036e89735fd43b
ubahnverleih <ubahnverleih@gmail.com>
2013-11-08 16:23:32 +0100

    Icons, DB + DVB Icons

---
+++ js/icons.js
@@ -0,0 +1,40 @@
+var DBicon = L.icon({
+	iconUrl: './img/operator/db_ag2.png',
+	iconRetinaUrl: './img/operator/db_a2@2x.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowUrl: './leaflet/images/marker-shadow.png',
+	shadowSize: [41, 41]
+});
+
+var DVBicon = L.icon({
+	iconUrl: './img/operator/dvb.png',
+	iconRetinaUrl: './img/operator/dvb@2x.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowUrl: './leaflet/images/marker-shadow.png',
+	shadowSize: [41, 41]
+});
+
+var DVBfixicon = L.icon({
+	iconUrl: './img/operator/dvb_fix.png',
+	iconRetinaUrl: './img/operator/dvb_fix@2x.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowUrl: './leaflet/images/marker-shadow.png',
+	shadowSize: [41, 41]
+});
+
+
+var StdIcon = L.icon({
+	iconUrl: './leaflet/images/marker-icon.png',
+	iconRetinaUrl: './leaflet/images/marker-icon@2x.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowUrl: './leaflet/images/marker-shadow.png',
+	shadowSize: [41, 41]
+});
--- js/parse_scripts.js
+++ js/parse_scripts.js
@@ -33,3 +33,36 @@ function stockwerk(value){
 	}
 	return result;
 }
+
+function preparseOperatorName(operator){
+	var result = operator;
+	var operator=operator.toLowerCase();
+	
+	//Operators
+	var DeutscheBahn = ['deutsche bahn', 'deutsche bahn ag', 'db', 'db ag', 'db bahn'];
+	var DVB_to_fix = ['dvb ag', 'dvb', 'dresdner verkehrsbetriebe ag'];
+	var DVB_right = ['dresdner verkehrsbetriebe'];
+
+	if (DeutscheBahn.indexOf(operator)>-1) result = 'deutsche bahn';
+	if (DVB_to_fix.indexOf(operator)>-1) result = 'DVB_to_fix';
+	if (DVB_right.indexOf(operator)>-1) result = 'DVB_right';
+
+	return result;
+}
+
+function getMarkerIcon(parsedOperator){
+	var result = StdIcon;
+	if (parsedOperator == "deutsche bahn") result = DBicon;
+	if (parsedOperator == "DVB_to_fix") result = DVBfixicon;
+	if (parsedOperator == "DVB_right") result = DVBicon;
+
+	return result;
+}
+
+
+
+
+
+
+
+
