655474fe81343890e3eb01708070a0ede91051ee
nw520 <me@nw520.de>
2018-05-27 23:49:14 +0200

    Added Saarbahn GmbH

--- js/icons.js
+++ js/icons.js
@@ -179,6 +179,16 @@ var SWUicon = L.icon({
 	shadowSize: [41, 41]
 });
 
+var SaarbahnIcon = L.icon({
+	iconUrl: './img/operator/saarbahn_gmbh.png',
+	iconRetinaUrl: './img/operator/saarbahn_gmbh@2x.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowUrl: './leaflet/images/marker-shadow.png',
+	shadowSize: [41, 41]
+});
+
 var StdUnknown = L.icon({
 	iconUrl: './img/operator/unknown.png',
 	iconRetinaUrl: './img/operator/unknown@2x.png',
